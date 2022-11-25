3a7d32b2be44e5f77a56088765d653bf763c86aa
Nikhil Sethi <sethi.nirvil@gmail.com>
2022-11-25 15:06:20 +0100

    update install script

--- tooling/install.sh
+++ tooling/install.sh
@@ -1,5 +1,7 @@
 pip3 install --upgrade pip
-cd ../gym-pybullet-drones/
+git submodule update --init --recursive
+cd gym-pybullet-drones
 sed -i 's/"1.9"/"^1.9"/g' pyproject.toml
 pip3 install -e .
-cd ../tooling
+cd ..
+
