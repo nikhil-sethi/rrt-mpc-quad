8752195c7e67fe0b17e2d63c3af84a734163dc25
Florian van Melis <F.J.vanMelis@student.tudelft.nl>
2023-01-09 15:21:23 +0100

    Modified maps 1 to 4 according to prelim

--- utils/color.py
+++ utils/color.py
@@ -3,8 +3,10 @@ import  enum
 class Color(enum.Enum):
     # RGBA tuple
     RED = (1,0,0,1)
+    REDGLASS = (0.8,0,0,0.8)
     GREEN = (0,1,0,1)
     BLUE = (0,0,1,1)
+    BLUEGLASS = (0,0,0.8,0.8)
     GRAY = (0.8,0.8,0.8,1)
-    GLASS = (0.8,0.8,0.8,0.7)
+    GLASS = (0.8,0.8,0.8,0.6)
     
