f73f3d2503d24e3edb036c573d8ed1ec3d24b899
Alex Ledbetter <alexander.h.ledbetter@student.tudelft.nl>
2023-01-11 16:04:34 +0100

    Updates to Evaluation.py

--- results/evaluation.py
+++ results/evaluation.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import numpy as np
 
 #to include subdirs as modules
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -7,22 +8,22 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from main import run
 
-def run_stat(n_evals = 10):
+def run_stat(n_evals = 1):
     ## metrics to evalutate. add whatever you want here
-    planner_ct = 0
-    planner_nodes = 0
-    planner_nodes_gc = 0
+    planner_ct = []
+    planner_nodes = []
+    planner_nodes_gc = []
     for seed in range(n_evals):
         res = run(seed = seed, gui=False, map_number=2)
-        planner_ct += res["global_planner"]["metrics"]["time"]
-        planner_nodes += res["global_planner"]["metrics"]["nodes_wo_gc"]
-        planner_nodes_gc += res["global_planner"]["metrics"]["nodes_w_gc"]
+        planner_ct.append(res["global_planner"]["metrics"]["time"])
+        planner_nodes.append(res["global_planner"]["metrics"]["nodes_wo_gc"])
+        planner_nodes_gc.append(res["global_planner"]["metrics"]["nodes_w_gc"])
 
 
     print( f"========== {n_evals} Evaluations ==========")
-    print("[Planner] Average computation time: ", planner_ct/n_evals)
-    print("[Planner] Average nodes: ", planner_nodes/n_evals)
-    print("[Planner] Average garbage collected nodes: ", planner_nodes_gc/n_evals)
+    print(f"[Planner] Mean and STD of Computation Time: {np.around(np.average(planner_ct), decimals=2)}, {np.around(np.std(planner_ct))}")
+    print(f"[Planner] Mean and STD of Number of Nodes: {np.around(np.average(planner_nodes))}, {np.around(np.std(planner_nodes))}")
+    print(f"[Planner] Mean and STD of Number of Garbage Collected Nodes: {np.around(np.average(planner_nodes_gc))}, {np.around(np.std(planner_nodes_gc))}")
     print("=============================================")
 
 def plot_graphs():
